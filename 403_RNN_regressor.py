d0f1e7e9591f2eb4c366dec4340a0fd4ee66c36e
Gladysgong <531404376@qq.com>
2018-01-22 17:41:18 -0800

    rnn_regressor

--- 403_RNN_regressor.py
+++ 403_RNN_regressor.py
@@ -4,12 +4,13 @@ from torch.autograd import Variable
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Hyper Parameters
 EPOCH = 1
 BATCH_SIZE = 64
 TIME_STEP = 28
-INPUT_SIZE = 28
+INPUT_SIZE = 1
 LR = 0.01
 DOWNLOAD_MNIST = False
 
@@ -25,43 +26,60 @@ class RNN(nn.Module):
     def __init__(self):
         super(RNN, self).__init__()
 
-        self.rnn = nn.LSTM(
-            input_size=INPUT_SIZE,
-            hidden_size=64,
+        self.rnn = nn.RNN(
+            input_size=INPUT_SIZE,  # 1
+            hidden_size=32,
             num_layers=1,
             batch_first=True,  # (batch,time_step,input)
         )
-        self.out = nn.Linear(64, 10)
+        self.out = nn.Linear(32, 1)
 
-    def forward(self, x):
-        r_out, (h_n, h_c) = self.rnn(x, None)
-        out = self.out(r_out[:, -1, :])  # (batch,time_step,input)
-        return out
+    def forward(self, x, h_state):
+        # x (batch,time_step,input)
+        # h_state (n_layers,batch,hidden_size)
+        # r_out (batch,time_step,hidden_size)
+
+        r_out, h_state = self.rnn(x, h_state)
+        outs = []
+        for time_step in range(r_out.size(1)):
+            outs.append(self.out(r_out[:, time_step, :]))
+        return torch.stack(outs, dim=1), h_state
 
 
 rnn = RNN()
 print(rnn)
 
 optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)
-loss_func = nn.CrossEntropyLoss()
+loss_func = nn.MSELoss
+()
 
-for epoch in range(EPOCH):
-    for step, (x, y) in enumerate(train_loader):
-        b_x = Variable(x.view(-1, 28, 28))
-        b_y = Variable(y)
-        output = rnn(b_x)
-        loss = loss_func(output, b_y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+h_state=None
 
-        if step % 50 == 0:
-            test_output = rnn(test_x)
-            pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
-            accuracy = sum(pred_y == test_y) / test_y.size
-            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data[0], '| test accuracy: %.2f' % accuracy)
+plt.figure(1, figsize=(12, 5))
+plt.ion()           # continuously plot
 
-test_output = rnn(test_x[:10].view(-1, 28, 28))
-pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
-print(pred_y, 'prediction number')
-print(test_y[:10], 'real number')
+for step in range(60):
+    start,end=step*np.pi,(step+1)*np.pi
+    steps=np.linspace(start,end,TIME_STEP,dtype=np.float32)
+    x_np=np.sin(steps)
+    y_np=np.cos(steps)
+    x=Variable(torch.from_numpy(x_np[np.newaxis,:,np.newaxis]))
+    y=Variable(torch.from_numpy(y_np[np.newaxis,:,np.newaxis]))
+
+    prediction,h_state=rnn(x,h_state)
+    h_state=Variable(h_state.data)
+
+    loss = loss_func(prediction,y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # plotting
+    plt.plot(steps, y_np.flatten(), 'r-')
+    plt.plot(steps, prediction.data.numpy().flatten(), 'b-')
+    plt.draw()
+    plt.pause(0.05)
+
+plt.ioff()
+plt.show()
+
